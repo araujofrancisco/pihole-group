7123b7e4f7a4bf8c0c80dda1888847514fac336e
araujofrancisco <franciscoa@localhost.localdomain>
2021-02-19 10:47:52 -0400

    Initial commit.

---
+++ pihole-group.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+readonly pihole="pihole_pihole.*"
+readonly database="/etc/pihole/gravity.db"
+
+print_usage() {
+	echo "Usage: pihole-group <enable|disable> <groupname>"
+	exit 0
+}
+
+if [[ $# -ne 2 ]]; then
+	print_usage
+fi
+
+groupname=$2
+
+case "${1}" in
+	"enable")  action=1;;
+	"disable") action=0;;
+	*) print_usage;;
+esac
+
+# build up the command to update the enabled setting for the group only if value differs
+# so this way we can return the number of rows affected
+cmd="update 'group' set enabled = $action where name = '$groupname' and enabled <> $action;select changes();"
+#echo $cmd
+
+# does execute the command on pihole container, container name might differ depending on setup
+# the location for the database might be difference as well
+docker exec $(docker ps | grep -o "$pihole") sqlite3 "$database" "$cmd"
+
